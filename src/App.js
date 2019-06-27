e962e0219973f48a60fe2f519910057f87efcf70
Mark Berney <berneydwmark@gmail.com>
2019-06-27 12:31:34 +1200

    Added App.js functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import AddVideo from '.components/AddVideo.js';
+import Displayer from '.components/Displayer.js';
+import Title from '.components/Title.js';
+import stylesheet from '.components/stylesheet.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+
+  }
+
+  addVideo(postSubmitted) {
+    this.setState(state => ({
+      posts: [postSubmitted]
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <Title title = {'Don\'t Laugh Challenge'}/>
+        <AddVideo onAddVideo = {(addedPost) => {
+          this.addVideo(addedPost);
+        }}/>
+        <div className = "video-wrapper">
+          <Displayer posts = {this.state.posts}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
