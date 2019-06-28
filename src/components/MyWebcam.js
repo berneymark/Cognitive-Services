7b422632202de3189b229c4305f0e0b6b21341b7
Mark Berney <berneydwmark@gmail.com>
2019-06-28 16:15:35 +1200

    convertByteArray and fetchData methods added

--- src/components/MyWebcam.js
+++ src/components/MyWebcam.js
@@ -2,15 +2,42 @@ import React from 'react';
 import Webcam from 'react-webcam';
 
 class MyWebcam extends React.Component {
+    constructor(props) {
+        super(props);
+        this.timerId = null;
+        this.isCapturing = false;
+    }
+    
     setRef = webcam => {
         this.webcam = webcam; 
     }
     
     startCapturing = () => {
-        setInterval(() => {
+        this.isCapturing = true;
+        this.timerId = setInterval(() => {
             const image = this.webcam.getScreenshot();
-            console.log(image);
+            const byteArrayImage = this.convertToByteArray(image);
+            this.fetchData(byteArrayImage);
         }, 1000);
+    }
+
+    convertToByteArray = (image) => {
+        const base64 = require('base64-js');
+        const base64string = image.split(',')[1];
+
+        return base64.toByteArray(base64string);
+    }
+
+    fetchData = (byteArray) => {
+        const apiKey = '5fc2fa40176b44e199807bc0a14b7478';
+        const apiEndpoint = 'https://australiaeast.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceAttributes=emotion';
+        fetch(apiEndpoint, {
+            body: byteArray,
+            headers: {'cache-control': 'no-cache', 'Ocp-Apim-Subscription-Key': apiKey, 'Content-Type': 'application/octet-stream'},
+            method: 'POST'
+        }).then(response => {
+            console.log(response.status);
+        })
     }
 
     render() {
